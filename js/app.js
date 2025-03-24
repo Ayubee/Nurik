350734c2e7e03cc6223253faa86eba16b995aadb
Ayubxon <aubsattorov@gmail.com>
2025-03-24 23:29:32 +0500

    Refactor header styles, update typography, and implement hero section with responsive design

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-const clickBtn = document.querySelector("#btn");
+const clickBtn = document.querySelector(".burger-checkbox");
 const burger = document.querySelector(".nav__list");
 clickBtn.addEventListener("click", () => {
   burger.classList.toggle("none");
+  console.log(1);
 });
