dfdb093035bda2650e6732efa302303b42a53c81
Ayubxon <aubsattorov@gmail.com>
2025-03-25 17:23:49 +0500

    Add FAQ page with interactive question toggles and update styles

---
+++ js/faq.js
@@ -0,0 +1,31 @@
+const click = document.querySelector(".faq__hero_box_q_1");
+const testclick = document.querySelector(".faq__hero__box__q__in__1");
+const click2 = document.querySelector(".faq__hero_box_q_2");
+const testclick2 = document.querySelector(".faq__hero__box__q__in__2");
+const click3 = document.querySelector(".faq__hero_box_q_3");
+const testclick3 = document.querySelector(".faq__hero__box__q__in__3");
+const click4 = document.querySelector(".faq__hero_box_q_4");
+const testclick4 = document.querySelector(".faq__hero__box__q__in__4");
+const click5 = document.querySelector(".faq__hero_box_q_5");
+const testclick5 = document.querySelector(".faq__hero__box__q__in__5");
+const click6 = document.querySelector(".faq__hero_box_q_6");
+const testclick6 = document.querySelector(".faq__hero__box__q__in__6");
+
+click.addEventListener("click", () => {
+  testclick.classList.toggle("faq__none");
+});
+click2.addEventListener("click", () => {
+  testclick2.classList.toggle("faq__none");
+});
+click3.addEventListener("click", () => {
+  testclick3.classList.toggle("faq__none");
+});
+click4.addEventListener("click", () => {
+  testclick4.classList.toggle("faq__none");
+});
+click5.addEventListener("click", () => {
+  testclick5.classList.toggle("faq__none");
+});
+click6.addEventListener("click", () => {
+  testclick6.classList.toggle("faq__none");
+});
