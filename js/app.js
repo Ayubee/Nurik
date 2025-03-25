3ae9a026ce97099068f8edc40e5fe801162af9ef
Ayubxon <aubsattorov@gmail.com>
2025-03-25 17:39:16 +0500

    Add new navigation list for educational links and update styles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const clickBtn = document.querySelector(".burger-checkbox");
-const burger = document.querySelector(".nav__list");
+const burger = document.querySelector(".nav__list__2");
 clickBtn.addEventListener("click", () => {
   burger.classList.toggle("none");
   console.log(1);
